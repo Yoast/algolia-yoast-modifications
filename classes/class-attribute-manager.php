49a75d500d2d8256a0ed65e5cd49267da67d7d29
Diede Exterkate <diede@yoast.com>
2016-11-10 17:32:57 +0100

    added documentation for the AttributeManager

--- classes/class-attribute-manager.php
+++ classes/class-attribute-manager.php
@@ -2,32 +2,64 @@
 
 namespace Yoast\YoastCom\AlgoliaModifications;
 
+/**
+ * Class Attribute_Manager
+ * Adds attributes to the Algolia index.
+ */
 class Attribute_Manager {
 
+	/**
+	 * Registers hooks.
+	 */
 	public function register_hooks() {
 		add_filter( 'algolia_searchable_post_shared_attributes', array( $this, 'add_excerpt_to_post' ), 10, 2 );
 		add_filter( 'algolia_searchable_post_shared_attributes', array( $this, 'add_metadesc_to_post' ), 10, 2 );
 		add_filter( 'algolia_searchable_post_shared_attributes', array( $this, 'add_author_url' ), 10, 2 );
 	}
 
+	/**
+	 * Adds the url of the author of the post to the Algolia index.
+	 *
+	 * @param array    $shared_attributes The attributes that are being indexed.
+	 * @param \WP_Post $post The post.
+	 *
+	 * @return array
+	 */
 	public function add_author_url( array $shared_attributes, \WP_Post $post ) {
 		$shared_attributes['post_author']['author_link'] = get_author_posts_url( $post->post_author, get_the_author_meta( 'nicename', $post->post_author ) );
 
 		return $shared_attributes;
 	}
 
+	/**
+	 * Adds the Yoast SEO metadescription of the post to the Algolia index.
+	 *
+	 * @param array    $shared_attributes The attributes that are being indexed.
+	 * @param \WP_Post $post The post.
+	 *
+	 * @return array
+	 */
 	public function add_metadesc_to_post( array $shared_attributes, \WP_Post $post ) {
 		$shared_attributes['metadesc'] = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
 
 		return $shared_attributes;
 	}
 
+	/**
+	 * Adds the exerpt of the post to the Algolia index.
+	 *
+	 * @param array    $shared_attributes The attributes that are being indexed.
+	 * @param \WP_Post $post The post.
+	 *
+	 * @return array
+	 */
 	public function add_excerpt_to_post( array $shared_attributes, \WP_Post $post ) {
 
 		$excerpt = apply_filters( 'the_excerpt', get_the_excerpt( $post->ID ) );
 		if ( is_string( $excerpt ) && ! empty( $excerpt ) ) {
 			$shared_attributes['excerpt'] = $excerpt;
-		} else {
+		}
+		else {
 			// todo: The content filter returned null for some reason. Use this filter once we know why that happend.
 //			$post_content                 = strip_shortcodes( apply_filters( 'the_content', $post->post_content ) );
 			$post_content                 = strip_shortcodes( $post->post_content );
